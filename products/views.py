17ec99ee70ea80e32fa9fd072aa27deaff8d7f95
Nathan Fisher <N.Fisher21@outlook.com>
2024-09-23 17:07:51 +0000

    Added sorting and product counts to product page

--- products/views.py
+++ products/views.py
@@ -16,24 +16,24 @@ def all_products(request):
 
     if request.GET:
         if 'sort' in request.GET:
-            sortkey= request.GET['sort']
+            sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
-                sortkey == 'lower_name'
+                sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-                
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-                
-        
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-        
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -44,6 +44,7 @@ def all_products(request):
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
@@ -56,6 +57,7 @@ def all_products(request):
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
+
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
